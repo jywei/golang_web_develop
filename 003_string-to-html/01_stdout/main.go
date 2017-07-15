88e5497eaad7357f6f4ac6d40efab76672b14521
Roy Wei <roywjy@gmail.com>
2017-07-15 17:43:06 +0800

    Apply basic string to HTML

---
+++ 003_string-to-html/01_stdout/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Roy Wei 2017"
+
+	tpl := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Hello World!</title>
+	</head>
+	<body>
+	<h1>` + name + `</h1>
+	</body>
+	</html>
+	`
+	fmt.Println(tpl)
+}
+
+// go run main.go > index.html
