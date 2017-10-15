09029af7d386c7f84b53ebb863a32c9cbb99faeb
Roy Wei <roywjy@gmail.com>
2017-10-15 15:52:17 +0800

    Fix typo

--- 008_func/02_data-formatting/main.go
+++ 008_func/02_data-formatting/main.go
@@ -10,7 +10,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gothml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
 }
 
 func monthDatYear(t time.Time) string {
