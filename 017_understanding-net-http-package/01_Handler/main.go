c6674d53802ee41bb6593049789e26c17de5aad2
Roy Wei <roywjy@gmail.com>
2017-11-19 18:46:26 +0800

    Add ParseForm example

--- 017_understanding-net-http-package/01_Handler/main.go
+++ 017_understanding-net-http-package/01_Handler/main.go
@@ -11,6 +11,9 @@ func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Any code you want in this func")
 }
 
+// hotdog is also a value to type hadler
+// hotdog implement Habdler interface now
+
 func main() {
 
 }
