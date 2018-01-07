cfe23fa11b861044b996a533f488a5395448e87d
Roy Wei <roywjy@gmail.com>
2018-01-07 19:11:32 +0800

    Add handons for default ServeMux

---
+++ 022_hands-on/01/02_solution/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", bar)
+	http.HandleFunc("/me/", myName)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "foo ran")
+}
+
+func bar(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "bar ran")
+}
+
+func myName(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "Hello Roy")
+}
